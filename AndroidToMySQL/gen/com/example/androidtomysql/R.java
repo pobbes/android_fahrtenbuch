b3d54fba076cdf918786bfbf931180033bf71fec
pobbes <b.pobbes@gmail.com>
2012-12-05 21:25:41 +0100

    changed app name

--- AndroidToMySQL/gen/com/example/androidtomysql/R.java
+++ AndroidToMySQL/gen/com/example/androidtomysql/R.java
@@ -33,7 +33,6 @@ public final class R {
         public static final int edit_user=0x7f030004;
         public static final int list_item=0x7f030005;
         public static final int main_screen=0x7f030006;
-        public static final int test=0x7f030007;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
