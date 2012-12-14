e3ad12ccf0f6ca42147651e5f43c217042a6c1d3
Benedikt <Benedikt@172.21.245.244>
2012-12-14 16:19:57 +0100

    CarLog oder was

---
+++ CarLog/gen/com/appclub/carlog/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.appclub.carlog;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ CarLog/src/com/appclub/carlog/LoginActivity.java
@@ -0,0 +1,106 @@
+package com.appclub.carlog;
+
+import java.util.HashMap;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+ 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+ 
+import mm.de.appclub.carlog.DatabaseHandler;
+import mm.de.appclub.carlog.UserFunctions;
+
+public class LoginActivity extends Activity
+{
+	Button btnLogin;
+    Button btnLinkToRegister;
+    EditText inputEmail;
+    EditText inputPassword;
+    TextView loginErrorMsg;
+ 
+    // JSON Response node names
+    private static String KEY_SUCCESS = "success";
+    private static String KEY_ERROR = "error";
+    private static String KEY_ERROR_MSG = "error_msg";
+    private static String KEY_USER_ID = "user_id";
+    private static String KEY_USERNAME = "username";
+    private static String KEY_EMAIL = "email";
+    private static String KEY_CREATED_AT = "created_at";
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+ 
+        // Importing all assets like buttons, text fields
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.password);
+        btnLogin = (Button) findViewById(R.id.sign_in_button);
+    //    btnLinkToRegister = (Button) findViewById(R.id.btnLinkToRegisterScreen);
+    //    loginErrorMsg = (TextView) findViewById(R.id.login_status_message);
+ 
+        // Login button Click Event
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+ 
+            public void onClick(View view) {
+                String email = inputEmail.getText().toString();
+                String password = inputPassword.getText().toString();
+                UserFunctions userFunction = new UserFunctions();
+                JSONObject json = userFunction.loginUser(email, password);
+ 
+                // check for login response
+                try {
+                    if (json.getString(KEY_SUCCESS) != null) {
+                        loginErrorMsg.setText("");
+                        String res = json.getString(KEY_SUCCESS);
+                        if(Integer.parseInt(res) == 1){
+                            // user successfully logged in
+                            // Store user details in SQLite Database
+                            DatabaseHandler db = new DatabaseHandler(getApplicationContext());
+                            JSONObject json_user = json.getJSONObject("user");
+ 
+                            // Clear all previous data in database
+                            userFunction.logoutUser(getApplicationContext());
+                            db.addUser(json_user.getString(KEY_USERNAME), json_user.getString(KEY_EMAIL), json.getString(KEY_USER_ID), json_user.getString(KEY_CREATED_AT));                        
+ 
+                            // Launch Dashboard Screen
+                            Intent dashboard = new Intent(getApplicationContext(), DashboardActivity.class);
+ 
+                            // Close all views before launching Dashboard
+                            dashboard.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(dashboard);
+ 
+                            // Close Login Screen
+                            finish();
+                        }else{
+                            // Error in login
+                            loginErrorMsg.setText("Incorrect username/password");
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+ 
+        // Link to Register Screen
+    //    btnLinkToRegister.setOnClickListener(new View.OnClickListener() {
+    // 
+    //        public void onClick(View view) {
+    //            Intent i = new Intent(getApplicationContext(),
+     //                   RegisterActivity.class);
+     //           startActivity(i);
+     //           finish();
+     //       }
+      //  });
+    }
+	
+	
+}
